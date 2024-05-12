eda74bda405d74b35dfb77b8cb1e9f74e205c010
babykumari <babykumarisahani1997@gmail.com>
2024-05-12 20:34:51 +0530

    Fetch News API Committed

--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ function FetchNews() {
   const [data, setData] = useState(null);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(true); // Initialize loading state to true
+  const apiKey = process.env.REACT_APP_API_KEY;
 
   useEffect(() => {
-    fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=1711f90fa4e744fab06e1f2bb4f1d565')
+    fetch(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}`)
       .then((response) => {
         if (!response.ok) {
           throw new Error('Network response was not ok');
@@ -23,7 +24,7 @@ function FetchNews() {
         setLoading(false);
       });
 
-  }, []);
+  }, [apiKey]);
 
   if (loading) {
     return <p>Loading...</p>;
@@ -39,36 +40,37 @@ function FetchNews() {
         <div className="max-w-7xl mx-auto px-4">
           <div className="flex justify-center">
             <div className="flex space-x-4">
-              <a href="/" className="text-white hover:text-gray-300 text-2xl">HOME</a>
+              <a href="/" className="text-white hover:text-gray-300 text-2xl">TOP HEADLINES - NEWS FEED</a>
             </div>
           </div>
         </div>
       </nav>
       <br />
-      <div className="flex justify-center">
-        <h1 className="text-2xl font-bold">Top headlines in the US</h1>
-      </div>
-      <br />
       <br />
       <div className="container mx-auto">
         {data && (
           <div className="flex justify-between">
-            {data.articles.slice(0, 5).map((article, index) => (
-              <div key={index} className="card bg-blue-100 p-4" style={{ width: "18rem" }}>
-                <img className="card-img-top" src={article.urlToImage} alt={article.title} />
-                <div className="card-body">
-                  <strong className="card-title">{article.title}</strong>
-                  <p className="card-text">{article.description}</p>
+            {data.articles
+              .filter(article => article.urlToImage) // Filter out articles without an image
+              .slice(0, 5)
+              .map((article, index) => (
+                <div key={index} className="card bg-blue-100 p-4" style={{ width: "18rem" }}>
+                  <a href={article.url} target="_blank" rel="noopener noreferrer">
+                  <p className="card-text text-left font-bold text-red-500">{article.source.name}: <span className="font-bold text-black">{new Date(article.publishedAt).toLocaleDateString()}</span></p>
+                    <img className="card-img-top" src={article.urlToImage} alt={article.title} />
+                    <div className="card-body">
+                      <strong className="card-title">{article.title}</strong>
+                      <p className="card-text">{article.description}</p>
+                      
+                    </div>
+                  </a>
                 </div>
-              </div>
-            ))}
+              ))}
           </div>
         )}
       </div>
     </div>
   );
 }
-
-
 
 export default FetchNews;
